71cb2d8f66aff5aa5815c0048b3807ac6bec02c8
SJKim <ksjksj0122@gmail.com>
2020-07-16 12:01:16 +0900

    Part 7, Add list_display, list_filter, search_fields, date_hierarchy to the ModelAdmin for Question

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -14,6 +14,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    date_hierarchy = 'pub_date'
 
 
 admin.site.register(Question, QuestionAdmin)
