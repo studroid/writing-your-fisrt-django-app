f5a1a7dae38f8be77e74c9d044ce5fe5765b75b8
SJKim <ksjksj0122@gmail.com>
2020-07-16 11:27:33 +0900

    Part 7, Use StackedInline to provide choices when adding or modifying Question data

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
